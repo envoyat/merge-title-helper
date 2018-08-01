83e1829b64da328ceff32f252249fbe9d24288ed
DominicFarrington <31464212+DominicFarrington@users.noreply.github.com>
2018-08-01 11:39:21 +1000

    Attach listener to window
    Move the listener to the window so the event will be caught correctly on dynamically inserted DOM elements

--- merge-title-helper.user.js
+++ merge-title-helper.user.js
@@ -46,5 +46,5 @@
     msgField.after(newDiv);
     isMergeMessageValid();
 
-    document.getElementById("merge_title_field").addEventListener("input", isMergeMessageValid);
+    window.addEventListener("input", isMergeMessageValid);
 })();
